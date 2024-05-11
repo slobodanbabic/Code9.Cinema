e886d2c6a6841d1d631664964053db3421b65cd5
slobodanbabic <slobodannbabic@gmail.com>
2024-05-11 11:40:35 +0200

    second comm

---
+++ Code9.API/Controllers/CinemaController.cs
@@ -0,0 +1,37 @@
+﻿using Code9.Domain.Models;
+using Code9.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Code9.API.Controllers
+{
+    [ApiController]
+    [Route("cinema")]
+    public class CinemaController : Controller
+    {
+
+        private readonly IMediator _mediator;
+
+        public CinemaController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cinema>>> getAllCinemas()
+        {
+            var cinemas = await _mediator.Send(new GetAllCinemaQuery());
+            return Ok(cinemas);
+        }
+}
+}
+
+
+
+ 
+
+   
+
+
+
