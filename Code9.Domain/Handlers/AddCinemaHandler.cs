4787250f9d83dfd4303b72193a9d88d13ee66505
slobodanbabic <slobodannbabic@gmail.com>
2024-05-11 13:00:10 +0200

    zavrsen zadatak 3 add cinema

---
+++ Code9.Domain/Handlers/AddCinemaHandler.cs
@@ -0,0 +1,36 @@
+﻿using Code9.Domain.Commands;
+using Code9.Domain.Interfaces;
+using Code9.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code9.Domain.Handlers
+{
+    public class AddCinemaHandler: IRequestHandler<AddCinemaCommand, Cinema>
+    {
+        private readonly ICinemaRepository _cinemaRepository;
+
+        public AddCinemaHandler(ICinemaRepository cinemaRepository)
+        {
+            _cinemaRepository = cinemaRepository;
+        }
+
+        public Task<Cinema> Handle(AddCinemaCommand request, CancellationToken cancellationToken)
+        {
+            var cinema = new Cinema {
+                City = request.City,
+                Name = request.Name,
+                Street = request.Street,
+                NumberOfAuditoriums = request.NumberOfAuditoriums
+
+            };
+            return  _cinemaRepository.AddCinema(cinema);
+        }
+    }
+
+  
+}
